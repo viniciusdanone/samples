d30f83bac86745fcc28da5b081efbd761277c1e2
Vinicius Silva <viniciusb@take.net>
2021-06-09 17:28:11 -0300

    update script

--- mysql/mysql-test.sh
+++ mysql/mysql-test.sh
@@ -6,21 +6,21 @@ MAX="4"
 
 fn_drop_database(){
 
-    mysql -h $MYSQL_SVC -uroot -p$MYSQL_PASSWORD --execute="drop database test2;"
-    mysql -h $MYSQL_SVC -uroot -p$MYSQL_PASSWORD --execute="drop database test4;"
-    mysql -h $MYSQL_SVC -uroot -p$MYSQL_PASSWORD --execute="drop database test6;"
-    mysql -h $MYSQL_SVC -uroot -p$MYSQL_PASSWORD --execute="drop database test8;"
-    mysql -h $MYSQL_SVC -uroot -p$MYSQL_PASSWORD --execute="drop database test10;"
+    mysql -uroot -p$MYSQL_PASSWORD --execute="drop database test2;"
+    mysql -uroot -p$MYSQL_PASSWORD --execute="drop database test4;"
+    mysql -uroot -p$MYSQL_PASSWORD --execute="drop database test6;"
+    mysql -uroot -p$MYSQL_PASSWORD --execute="drop database test8;"
+    mysql -uroot -p$MYSQL_PASSWORD --execute="drop database test10;"
 
 }
 
 fn_create_database(){
 
-    mysql -h $MYSQL_SVC -uroot -p"$MYSQL_PASSWORD" --execute="create database test2;"
-    mysql -h $MYSQL_SVC -uroot -p"$MYSQL_PASSWORD" --execute="create database test4;"
-    mysql -h $MYSQL_SVC -uroot -p"$MYSQL_PASSWORD" --execute="create database test6;"
-    mysql -h $MYSQL_SVC -uroot -p"$MYSQL_PASSWORD" --execute="create database test8;"
-    mysql -h $MYSQL_SVC -uroot -p"$MYSQL_PASSWORD" --execute="create database test10;"
+    mysql -uroot -p"$MYSQL_PASSWORD" --execute="create database test2;"
+    mysql -uroot -p"$MYSQL_PASSWORD" --execute="create database test4;"
+    mysql -uroot -p"$MYSQL_PASSWORD" --execute="create database test6;"
+    mysql -uroot -p"$MYSQL_PASSWORD" --execute="create database test8;"
+    mysql -uroot -p"$MYSQL_PASSWORD" --execute="create database test10;"
 }
 
 fn_run_test(){
