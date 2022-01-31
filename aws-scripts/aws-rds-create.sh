7752d74d2cac356969eb698ffcdf6e4e587d04f7
Vinicius Batista <viniciusb@chatfood.io>
2022-01-31 17:20:37 -0300

    aws: rds restore script

--- aws-scripts/aws-rds-create.sh
+++ aws-scripts/aws-rds-create.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
-NEW_VERSION="5-7-01"
+ENGINE_VERSION="5.7.36"
 RDS_DESTINATION="chatlab-migration-primary"
-RDS_SOURCE="chatlab-stg-read"
+RDS_SOURCE="chatlab-stg"
 RDS_SNAPSHOT="rds:chatlab-stg-2022-01-30-23-07"
 RDS_SIZE="db.t2.small"
 
-aws rds restore-db-instance-from-db-snapshot \
+fn_snapshot_restore (){
+echo "Starting restore from snampshot $RDS_SNAPSHOT, wait and take a coffe..."    
+time aws rds restore-db-instance-from-db-snapshot \
+    --engine mysql \
+    --no-multi-az \
+    --publicly-accessible \
     --db-instance-identifier $RDS_DESTINATION \
     --db-snapshot-identifier $RDS_SNAPSHOT \
-    --publicly-accessible \
-    --db-instance-class $RDS_SIZE --dry-run
+    --db-instance-class $RDS_SIZE \
+    --wait
+}
 
+fn_instance_upgrade(){
+echo "Starting upgrade of instance $RDS_DESTINATION"
+time aws rds modify-db-instance \
+    --db-instance-identifier $RDS_DESTINATION \
+    --backup-retention-period 7 \
+    --engine-version $ENGINE_VERSION \
+    --apply-immediately \
+    --wait
+}
 
-#aws rds describe-db-snapshots --db-snapshot-identifier rds:chatlab-stg-2022-01-30-23-07 
+fn_instance_list(){
+raws ds describe-db-instances \
+    --db-instance-identifier $RDS_DESTINATION
+}
+
+case "$1" in
+ 'restore')
+   fn_snapshot_restore;;
+ 'upgrade')
+   fn_instance_upgrade;;
+ 'describe')
+   fn_instance_list;;
+ *)
+       echo "Usage: $0 {restore|upgrade|describe}"
+esac
